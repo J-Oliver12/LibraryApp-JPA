ce8474897abb222faaece846e6a539736bd0449e
Juan Oliver <juan_oliver@live.se>
2023-10-18 11:11:56 +0200

    Initial commit

---
+++ src/test/java/com/example/libraryappjpa/LibraryAppJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryappjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAppJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
